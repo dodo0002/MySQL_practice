449ca01878b9e744360bfa8609254d03d34581bc
DoDo0002 <118946887+DoDo0002@users.noreply.github.com>
2025-06-07 15:59:46 +0800

    Improve SQL skills
    About
    UPDATE table_name SET *** CASE ***
        WHEN *** THEN  ***
    CONCAT()

--- library_practice.sql
+++ library_practice.sql
@@ -1,3 +1,4 @@
+--2025/06/06
 --創建一個名為books的表格
 -- CREATE TABLE books(
 --     id int PRIMARY KEY AUTO_INCREMENT,
@@ -124,17 +125,97 @@
 -- SET author_id = 5
 -- WHERE book_id IN(7);
 
-INSERT INTO Members(first_name, last_name, email)
-VALUES("Alice", "Chen", "alice.chen@example.com"),
-("Bob", "Lin", "bob.lin@expamle.com"),
-("Carol", "Wang", "carol.wang@example.com");
+--添加Members資料
+-- INSERT INTO Members(first_name, last_name, email)
+-- VALUES("Alice", "Chen", "alice.chen@example.com"),
+-- ("Bob", "Lin", "bob.lin@expamle.com"),
+-- ("Carol", "Wang", "carol.wang@example.com");
 
-INSERT INTO Loans(book_id, member_id, loan_date, return_date)
-VALUES(1, 1, '2025-05-01', '2025-05-15'),
-(3, 2, '2025-05-10', NULL),
-(5, 1, '2025-05-20', NULL),
-(2, 3, '2025-05-25', '2025-06-03'),
-(7, 2, '2025-06-01', NULL);
+--添加Loans資料
+-- INSERT INTO Loans(book_id, member_id, loan_date, return_date)
+-- VALUES(1, 1, '2025-05-01', '2025-05-15'),
+-- (3, 2, '2025-05-10', NULL),
+-- (5, 1, '2025-05-20', NULL),
+-- (2, 3, '2025-05-25', '2025-06-03'),
+-- (7, 2, '2025-06-01', NULL);
+
+
+--2025/06/07
+-- 新增欄位
+-- ALTER TABLE books
+-- ADD COLUMN (pages INT,
+-- rating DECIMAL(3, 2));
+
+-- MODIFY只有改資料型態 CHANGE是改欄位名稱
+-- ALTER TABLE Members 
+-- MODIFY COLUMN email VARCHAR(150);
+
+-- 一次新增多筆資料，SET 欄位要等於甚麼 CASE condition欄位
+--     WHEN condition THEN 要新增的資料
+-- UPDATE books
+-- SET pages = CASE book_id 
+--     WHEN 1 THEN 320
+--     WHEN 2 THEN 328
+--     WHEN 3 THEN 447
+--     WHEN 4 THEN 279
+--     WHEN 5 THEN 112
+--     WHEN 6 THEN 256
+--     WHEN 7 THEN 253
+--     WHEN 8 THEN 192
+--     WHEN 9 THEN 1138
+--     WHEN 10 THEN 474
+--     WHEN 11 THEN 255
+--     WHEN 12 THEN 193
+-- END,  --要加END跟逗號
+--      rating = CASE book_id
+--     WHEN 1 THEN 4.85
+--     WHEN 2 THEN 4.78
+--     WHEN 3 THEN 4.55
+--     WHEN 4 THEN 4.60
+--     WHEN 5 THEN 4.70
+--     WHEN 6 THEN 4.30
+--     WHEN 7 THEN 4.45
+--     WHEN 8 THEN 4.75
+--     WHEN 9 THEN 4.65
+--     WHEN 10 THEN 4.20
+--     WHEN 11 THEN 4.35
+--     WHEN 12 THEN 4.90
+-- END;
+
+-- LIKE加% 是不指定字串的意思 只要包含即可 放在前面就是前面不指定 後面就是後面不指定
+-- SELECT * FROM books
+-- WHERE genre LIKE "%Fiction%" AND published_year >= 2000
+-- ORDER BY title ASC;
+
+-- 查詢 然後排序
+-- SELECT * FROM Authors
+-- WHERE birth_year < 1950
+-- ORDER BY birth_year DESC;
+
+-- JOIN + CONCAT使用 注意括號內非加號 是逗號
+-- SELECT  b.title AS title, CONCAT(m.last_name , " " , m.first_name ) AS full_name, l.loan_date FROM Loans l
+-- JOIN books b
+-- ON l.book_id = b.book_id
+-- JOIN Members m
+-- ON l.member_id = m.member_id
+-- WHERE return_date IS NULL OR return_date > '2025-06-07';
+
+-- 之後可以練習用看看BETWEEN
+-- SELECT title, published_year FROM books
+-- WHERE  published_year >= 1980 AND published_year <= 2000 ;
+
+-- INSERT INTO Authors(first_name, last_name)
+-- VALUES("J.K.", "Rowling");
+
+-- UPDATE books
+-- SET author_id = 7
+-- WHERE book_id = 1;
+
+SELECT CONCAT(a.first_name," ", a.last_name) AS author_full_name, COUNT(b.author_id) AS total FROM books b
+JOIN Authors a
+ON b.author_id = a.author_id 
+GROUP BY CONCAT(a.first_name," ", a.last_name)
+ORDER BY COUNT(author_id) DESC;
 
 -- DESCRIBE Authors;
 -- DESCRIBE Members;
