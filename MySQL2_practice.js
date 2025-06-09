1803af3c89bcc73cf12be6fbdf0dc64315f0f2cb
DoDo0002 <118946887+DoDo0002@users.noreply.github.com>
2025-06-09 22:00:52 +0800

    MySQL2 + node.js
    practice about integrate

---
+++ MySQL2_practice.js
@@ -0,0 +1,38 @@
+import mysql from "mysql2/promise";
+
+const connection = await mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "doom",
+});
+
+try {
+  console.log("已成功連接到資料庫!");
+
+  //   const [results, fields] = await connection.query(
+  //     " SELECT * FROM `employees` WHERE  `age` > 25"
+  //   );
+
+  //   connection.query("CREATE DATABASE test_db"),
+  //     function (err, result) {
+  //       if (err) throw err;
+  //       console.log("資料庫建立成功!");
+  //     };
+
+  //   const [table] = await connection.query(
+  //     "CREATE TABLE users(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50), age INT, email VARCHAR(150))"
+  //   );
+  //   console.log("TABLE建立成功!");
+
+  const [results, field] = await connection.query(
+    "INSERT INTO users VALUES(1, 'Jim', 27, 'travel123456@gmail.com')"
+  );
+  console.log(results);
+  console.log("資料新增成功! 恭喜");
+
+  //   console.log(results);
+  //   console.log(fields);
+} catch (err) {
+  console.log(err);
+}
